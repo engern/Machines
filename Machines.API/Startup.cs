1ebb6e8ea1688fb729e593a081dc6f490798b18f
Tommy Lind Enger <tommy@abacus-it.no>
2021-08-16 18:04:08 +0200

    Add swagger

--- Machines.API/Startup.cs
+++ Machines.API/Startup.cs
@@ -26,6 +26,7 @@ namespace Machines.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,14 @@ namespace Machines.API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Machines API");
+                c.RoutePrefix = string.Empty;
+            });
+
         }
     }
 }
--- Machines.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Machines.Models
-{
-    public class Class1
-    {
-    }
-}
