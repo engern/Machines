411966674a7eb00a6e584f84a4ca61c64abaf71f
Tommy Lind Enger <tommy@abacus-it.no>
2021-08-19 09:13:33 +0200

    refactor test class

--- test/Machines.GuiTest/MachineTests.cs
+++ test/Machines.GuiTest/MachineTests.cs
@@ -22,55 +22,55 @@ namespace Machines.GuiTest
         }
 
         [Test]
-        public void test_machine()
+        public void AddEditAndDeleteMachine()
         {
             driver.Url = test_url;
 
-            var machinename = Guid.NewGuid().ToString();
+            var originalMachineName = Guid.NewGuid().ToString();
+            var updatedMachineName = Guid.NewGuid().ToString();
 
             // add new machine
             driver.FindElement(By.Id("BtnAdd")).Click();
-            driver.FindElement(By.Id("InputMachineName")).SendKeys(machinename);
+            driver.FindElement(By.Id("InputMachineName")).SendKeys(originalMachineName);
             driver.FindElement(By.Id("BtnSaveMachine")).Click();
 
-            // Verify item in list
-            driver.FindElement(By.XPath($"//*[text()='{machinename}']"));
+            // Verify item in list, will throw exception and fail test if not present
+            driver.FindElement(By.XPath($"//*[text()='{originalMachineName}']"));
 
             // edit item
-            var editButton = driver.FindElement(By.XPath($"//div[@data-machinename='{machinename}']/h3/i[@class='fas fa-pen']"));
+            var editButton = driver.FindElement(By.XPath($"//div[@data-machinename='{originalMachineName}']/h3/i[@class='fas fa-pen']"));
             editButton.Click();
 
-            var newMachineName = Guid.NewGuid().ToString();
             
-            var editInput = driver.FindElement(By.XPath($"//div[@data-machinename='{machinename}']/h3/input"));
+            var editInput = driver.FindElement(By.XPath($"//div[@data-machinename='{originalMachineName}']/h3/input"));
             editInput.Clear();
-            editInput.SendKeys(newMachineName);
+            editInput.SendKeys(updatedMachineName);
 
-            var saveButton = driver.FindElement(By.XPath($"//div[@data-machinename='{machinename}']/h3/i[@class='fas fa-save']"));
+            var saveButton = driver.FindElement(By.XPath($"//div[@data-machinename='{originalMachineName}']/h3/i[@class='fas fa-save']"));
             saveButton.Click();
 
-            // Verify new name in list
-            driver.FindElement(By.XPath($"//*[text()='{newMachineName}']"));
+            // Verify new name in list, will throw exception and fail test if not present
+            driver.FindElement(By.XPath($"//*[text()='{updatedMachineName}']"));
 
             //verify old name removed
             try
             {
-                driver.FindElement(By.XPath($"//*[text()='{machinename}']"));
+                driver.FindElement(By.XPath($"//*[text()='{originalMachineName}']"));
                 Assert.Fail("original item is still present"); // If it does not throw exception, item is still in list and the test should fail
             }
             catch { }
 
             // delete item
-            var deleteButton = driver.FindElement(By.XPath($"//div[@data-machinename='{newMachineName}']/h3/i[@class='fas fa-times']"));
+            var deleteButton = driver.FindElement(By.XPath($"//div[@data-machinename='{updatedMachineName}']/h3/i[@class='fas fa-times']"));
             deleteButton.Click();
 
             driver.SwitchTo().Alert().Accept();
             Thread.Sleep(500); // Give the GUI some time to update
 
-            //verify item removed
+            //verify item delted
             try
             {
-                driver.FindElement(By.XPath($"//*[text()='{newMachineName}']"));
+                driver.FindElement(By.XPath($"//*[text()='{updatedMachineName}']"));
                 Assert.Fail("updated item is still present"); // If it does not throw exception, item is still in list and the test should fail
             }
             catch { }
